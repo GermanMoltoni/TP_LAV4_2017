336e8fb6fc3142aa752bbcc2b0844d7757f51a5b
Marcelo <programacion@informaticasa.com.ar>
2017-11-08 11:00:33 -0300

    cambio ruta

--- src/app/servicios/jugador/jugador.service.ts
+++ src/app/servicios/jugador/jugador.service.ts
@@ -8,7 +8,7 @@ export class JugadorService {
 
   constructor(private http:MiHttpService) { }
   Crear(path:string,jugador:Jugador){
-    this.http.Post(this.url+path,{usuario:jugador.usuario,sexo:jugador.sexo,mail:jugador.email,password:jugador.password});
+    this.http.Post(this.url+path,{usuario:jugador.usuario,sexo:jugador.sexo,mail:jugador.email,password:jugador.password}).subscribe(msg=>{console.log(msg)});
     
   }
   Login(path:string,data:{usuario:string,password:string}){
